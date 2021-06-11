6c4fbdca50e5a18cf9b4c3718fde9d1bcb54c227
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-11 17:42:45 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        hello react native from uzair iqbal
+      </Text>
+    </View>
+  );
+};
+
+export default App;
