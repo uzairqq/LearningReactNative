6c2db85952f2bec05e901c13673da29aa8fe3816
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-12 13:55:22 +0500

    passing props to a component

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Home from './components/Home';
 import Login from './components/Login';
 
 const App = () => {
+  let data = "Some Amount of data in PROPS"
   return (
     <View>
       <Text>Hello From React Native</Text>
-      <Login />
+      {/* <Login /> */}
+      <Home data={data} />
     </View>
   );
 };
