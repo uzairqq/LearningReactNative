41cdbfc342149745668420fa38dd8a8bec85bb48
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-12 12:28:42 +0500

    Classe Based Component With Button

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 
-const Login = () => {
-    return (
-        <View>
-            <Text>
-                Login Screens
-            </Text>
-            <Button title="Login" onPress={() => alert("Hi, This is Function Component Login Button")}>Login</Button>
-        </View >
-    )
+class Login extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    This is Class Based Component
+                </Text>
+                <Button title="Login Click Me!!" onPress={() => alert("This is Login Class Based Component")} />
+            </View>
+        )
+    }
 }
 
 export default Login;
