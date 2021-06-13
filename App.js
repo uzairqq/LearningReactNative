2ad5e9f3969068de1422531bd6a438ac50cebba7
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-13 16:05:54 +0500

    Working with State in class based component

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import Home from './components/Home';
-import Login from './components/Login';
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
 
-const App = () => {
-  let data = "Some Amount of data in PROPS"
-  return (
-    <View>
-      <Text>Hello From React Native</Text>
-      {/* <Login /> */}
-      <Home data={data} />
-    </View>
-  );
-};
+
+
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: "App Some Data"
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          {this.state.data}
+        </Text>
+        <Button title="Update State" onPress={() => this.setState({ data: "Change State Data" })}></Button>
+      </View >
+    )
+  }
+}
+
 
 export default App;
