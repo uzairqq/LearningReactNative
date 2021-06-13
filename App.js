525de44a46c4fa23d55cadb2cde368be70173b1f
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-13 16:29:35 +0500

    working with In-Line Styling

--- App.js
+++ App.js
@@ -3,27 +3,16 @@ import { View, Text, Button } from 'react-native';
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: "App State Data"
-    }
-  }
-
-
-  changeStateMethod() {
-    this.setState({ data: "Update State data" })
-  }
   render() {
-
-
     return (
 
       <View>
-        <Text>
-          {this.state.data}
+        <Text style={{ color: 'red' }}>
+          Text 1
         </Text>
-        <Button title="Please Change State" onPress={() => this.changeStateMethod()}></Button>
+        <Text style={{ fontSize: 30 }}>
+          Text 2
+        </Text>
       </View>
     )
   }
