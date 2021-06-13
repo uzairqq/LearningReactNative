3b50d61535c9b8438815027dadbd74d360a03999
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-13 16:12:15 +0500

    Working with State in Class based component with method

--- App.js
+++ App.js
@@ -2,29 +2,33 @@ import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 
 
-
-
-
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      data: "App Some Data"
+      data: "App State Data"
     }
   }
 
+
+  changeStateMethod() {
+    this.setState({ data: "Update State data" })
+  }
   render() {
+
+
     return (
+
       <View>
         <Text>
           {this.state.data}
         </Text>
-        <Button title="Update State" onPress={() => this.setState({ data: "Change State Data" })}></Button>
-      </View >
+        <Button title="Please Change State" onPress={() => this.changeStateMethod()}></Button>
+      </View>
     )
   }
 }
+
 
 
 export default App;
