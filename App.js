60ebd5bf1d94902c8fcb0abf62573609838f6d92
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-17 19:59:01 +0500

    Using Text Input with State

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: ""
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>Handling Text Input</Text>
+        <Text>{this.state.name}</Text>
         <TextInput placeholder={"Enter your name"}
-          onChangeText={(e) => { console.warn(e) }}
+          onChangeText={(e) => { this.setState({ name: e }) }}
         ></TextInput>
       </View>
 
