eaa98968da501486cce9154de97fb5e8a1f82edf
Uzair Iqbal <uzair.qq@outlook.com>
2021-06-17 19:56:56 +0500

    Using the Text Input

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, Text, TextInput } from 'react-native';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <Text style={{ color: 'red', backgroundColor: 'green' }}>Text 1</Text>
-        <Text style={styles.text2}>Text 2</Text>
-        <Text style={[styles.color, styles.font]}>Text 3</Text>
-      </View >
+        <Text>Handling Text Input</Text>
+        <TextInput placeholder={"Enter your name"}
+          onChangeText={(e) => { console.warn(e) }}
+        ></TextInput>
+      </View>
+
+
     )
   }
 }
-
-const styles = StyleSheet.create({
-  text2: {
-    color: 'blue',
-    backgroundColor: 'yellow'
-  },
-
-  color: {
-    backgroundColor: 'blue',
-    color: 'red'
-  },
-  font: {
-    fontSize: 30,
-    fontStyle: 'normal'
-  }
-})
-
-
 
 
 export default App;
