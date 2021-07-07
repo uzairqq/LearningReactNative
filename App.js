d4163e3ff3abb615a8a1855d2ca1a1d29c58e696
Uzair Iqbal <uzair.qq@outlook.com>
2021-07-07 17:58:16 +0500

    Full Form With Button,TextBox,State and Styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 class App extends Component {
   constructor() {
@@ -10,6 +10,9 @@ class App extends Component {
       address: ''
     }
   }
+  submit() {
+    console.warn(this.state);
+  }
 
 
   render() {
@@ -18,22 +21,35 @@ class App extends Component {
         <Text>Simple Form</Text>
         <TextInput placeholder="Enter Name"
           onChangeText={(text) => { this.setState({ name: text }) }}
-        // style={styles.textBox}
+          style={styles.textBox}
         />
 
         <TextInput placeholder="Enter Password"
           onChangeText={(text) => { this.setState({ password: text }) }}
-        // style={styles.textBox}
+          secureTextEntry={true}
+          style={styles.textBox}
         />
 
         <TextInput placeholder="Enter Address"
           onChangeText={(text) => { this.setState({ address: text }) }}
-        // style={styles.textBox}
+          style={styles.textBox}
         />
+
+        <Button onPress={() => { this.submit() }} title="Submit" />
       </View>
     )
   }
 }
 
+
+const styles = StyleSheet.create({
+  textBox: {
+    borderColor: 'skyblue',
+    borderWidth: 2,
+    padding: 10,
+    marginHorizontal: 20,
+    marginVertical: 20
+  }
+})
 
 export default App;
