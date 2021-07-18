d15a60e4b898b82dd1b500893cd4d92be3c4b5b3
Uzair Iqbal <uzair.qq@outlook.com>
2021-07-18 15:38:04 +0500

    using flex

--- App.js
+++ App.js
@@ -4,52 +4,18 @@ import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      password: '',
-      address: ''
-    }
-  }
-  submit() {
-    console.warn("all values", this.state);
-  }
+
 
 
   render() {
     return (
-
-      <View>
-        <Text>SIMPLE FORM</Text>
-        <TextInput placeholder="Enter Name"
-          onChangeText={(text) => { this.setState({ name: text }) }}
-          style={styles.textBox}
-        />
-        <TextInput placeholder="Enter Password"
-          secureTextEntry={true}
-          onChangeText={(text) => { this.setState({ password: text }) }}
-          style={styles.textBox}
-        />
-        <TextInput placeholder="Enter Address"
-          onChangeText={(text) => { this.setState({ address: text }) }}
-          style={styles.textBox}
-        />
-        <Button title="Submit" onPress={() => this.submit()} />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 50, backgroundColor: 'red' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'green' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
       </View>
     )
   }
 }
-
-
-const styles = StyleSheet.create({
-  textBox: {
-    borderColor: 'skyblue',
-    borderWidth: 2,
-    padding: 10,
-    marginHorizontal: 20,
-    marginVertical: 20
-  }
-})
 
 export default App;
