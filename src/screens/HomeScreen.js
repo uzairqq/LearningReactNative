52eb0b55a13de37a6f184fa2040cc28ecc8f7558
Uzair Iqbal <uzair.qq@outlook.com>
2022-06-11 19:17:53 +0500

    Using Different Datatypes variables in a View Text

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,9 +3,16 @@ import { View, Text, StyleSheet } from "react-native";
 import ComponentScreen from "./ComponentScreen";
 
 const HomeScreen = () => {
+  const greeting = <Text>Hi There</Text>;
+  const greeting1 = "Hi There 2";
+  const arrayCheck = ["123", "456"];
+
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
+      {greeting}
+      <Text>{greeting1}</Text>
+      <Text>{arrayCheck}</Text>
       <ComponentScreen />
     </View>
   );
