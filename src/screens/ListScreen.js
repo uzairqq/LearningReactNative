c8f53d2f774ff2e03525b7faef3b38de070c98cc
Uzair Iqbal <uzair.qq@outlook.com>
2022-06-11 22:47:23 +0500

    Key Method 2, KeyExtractor on List

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,19 +3,20 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const data = [
-    { name: "friend#1", key: 1 },
-    { name: "friend#2", key: 2 },
-    { name: "friend#3", key: 3 },
-    { name: "friend#4", key: 4 },
-    { name: "friend#5", key: 5 },
-    { name: "friend#6", key: 6 },
-    { name: "friend#7", key: 7 },
-    { name: "friend#8", key: 8 },
+    { name: "friend#1" },
+    { name: "friend#2" },
+    { name: "friend#3" },
+    { name: "friend#4" },
+    { name: "friend#5" },
+    { name: "friend#6" },
+    { name: "friend#7" },
+    { name: "friend#8" },
   ];
 
   return (
     <View>
       <FlatList
+        keyExtractor={(freind) => freind.name}
         data={data}
         renderItem={(element) => {
           return <Text key={element.item.key}>{element.item.name}</Text>;
