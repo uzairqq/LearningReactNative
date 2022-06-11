ea8342d0c0ac89fbbbc7435c508d60c1398b69b3
Uzair Iqbal <uzair.qq@outlook.com>
2022-06-11 22:43:36 +0500

    Dealing with Keys Method 1

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,14 +3,14 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const data = [
-    { name: "friend#1" },
-    { name: "friend#2" },
-    { name: "friend#3" },
-    { name: "friend#4" },
-    { name: "friend#5" },
-    { name: "friend#6" },
-    { name: "friend#7" },
-    { name: "friend#8" },
+    { name: "friend#1", key: 1 },
+    { name: "friend#2", key: 2 },
+    { name: "friend#3", key: 3 },
+    { name: "friend#4", key: 4 },
+    { name: "friend#5", key: 5 },
+    { name: "friend#6", key: 6 },
+    { name: "friend#7", key: 7 },
+    { name: "friend#8", key: 8 },
   ];
 
   return (
@@ -18,7 +18,7 @@ const ListScreen = () => {
       <FlatList
         data={data}
         renderItem={(element) => {
-          return <Text>{element.item.name}</Text>;
+          return <Text key={element.item.key}>{element.item.name}</Text>;
         }}
       />
     </View>
