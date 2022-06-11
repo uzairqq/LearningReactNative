603ab1fc8388b61676a6a4f02fbb6bba4bbc51cb
Uzair Iqbal <uzair.qq@outlook.com>
2022-06-11 22:10:49 +0500

    Rendering a List Using FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
+  const data = [
+    { name: "friend#1" },
+    { name: "friend#2" },
+    { name: "friend#3" },
+    { name: "friend#4" },
+    { name: "friend#5" },
+    { name: "friend#6" },
+    { name: "friend#7" },
+    { name: "friend#8" },
+  ];
+
   return (
     <View>
-      <Text style={styles.textStyle}>List Screen</Text>
+      <FlatList
+        data={data}
+        renderItem={(element) => {
+          return <Text>{element.item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
