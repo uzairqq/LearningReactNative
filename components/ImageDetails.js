c26fe7bd752cf6748700e5c20c92fa77f7b37c64
Uzair Iqbal <uzair.qq@outlook.com>
2022-06-17 18:42:31 +0500

    Passing Image Score From Image to Image Details EXCERCISE

--- components/ImageDetails.js
+++ components/ImageDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 
-const ImageDetails = ({ title, imageSource }) => {
+const ImageDetails = ({ title, imageSource, imageScore }) => {
   return (
     <View>
       <Image source={imageSource} />
       <Text>{title}</Text>
+      <Text>Image Score - {imageScore}</Text>
     </View>
   );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -8,14 +8,17 @@ const ImageScreen = () => {
       <ImageDetails
         title="Forest"
         imageSource={require("../../assets/forest.jpg")}
+        imageScore={"9"}
       />
       <ImageDetails
         title="Mountain"
         imageSource={require("../../assets/mountain.jpg")}
+        imageScore={"7"}
       />
       <ImageDetails
         title="Beach"
         imageSource={require("../../assets/beach.jpg")}
+        imageScore={"4"}
       />
     </View>
   );
