1952472c7448eee971ed0aab7fe4cae9fb1f11db
Uzair Iqbal <uzair.qq@outlook.com>
2022-06-25 04:22:42 +0500

    Generating Random Colors in ColorsScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -5,8 +5,18 @@ const ColorScreen = () => {
   return (
     <View>
       <Button title="Add a Color" />
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+      ></View>
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.random() * 256;
+  const green = Math.random() * 256;
+  const blue = Math.random() * 256;
+  return `rgb(${red},${blue},${green})`;
 };
 
 const styles = StyleSheet.create({});
